eabd10f0b024207cbcf3985a4a10076578c990fb
WarFW <106368492+WarFW@users.noreply.github.com>
2020-12-26 11:26:03 +0000

    CMakeLists instead of Makefile

---
+++ TextClassification/imp_by_tensorflow2/TextBiRNN/main.py
@@ -0,0 +1,79 @@
+
+# -*- coding: utf-8 -*-
+# @Time : 2020/4/21 11:44
+# @Author : zdqzyx
+# @File : main.py
+# @Software: PyCharm
+
+
+# ===================== set random  ===========================
+import numpy as np
+import tensorflow as tf
+import random as rn
+np.random.seed(0)
+rn.seed(0)
+tf.random.set_seed(0)
+# =============================================================
+
+import os
+from tensorflow.keras.callbacks import EarlyStopping, TensorBoard, ModelCheckpoint
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from TextClassification.imp_by_tensorflow2.TextBiRNN.text_birnn import TextBiRNN
+
+def checkout_dir(dir_path, do_delete=False):
+    import shutil
+    if do_delete and os.path.exists(dir_path):
+        shutil.rmtree(dir_path)
+    if not os.path.exists(dir_path):
+        print(dir_path, 'make dir ok')
+        os.makedirs(dir_path)
+
+
+class ModelHepler:
+    def __init__(self, class_num, maxlen, max_features, embedding_dims, epochs, batch_size):
+        self.class_num = class_num
+        self.maxlen = maxlen
+        self.max_features = max_features
+        self.embedding_dims = embedding_dims
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.callback_list = []
+        print('Bulid Model...')
+        self.create_model()
+
+    def create_model(self):
+        model = TextBiRNN(maxlen=self.maxlen,
+                         max_features=self.max_features,
+                         embedding_dims=self.embedding_dims,
+                         class_num=self.class_num,
+                         last_activation='softmax',
+                          dense_size=[128, 64]
+                          )
+        model.compile(
+            optimizer='adam',
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+            metrics=['accuracy'],
+        )
+
+        model.build_graph(input_shape=(None, self.maxlen))
+        model.summary()
+        self.model =  model
+
+    def get_callback(self, use_early_stop=True, tensorboard_log_dir='logs\\FastText-epoch-5', checkpoint_path="save_model_dir\\cp-moel.ckpt"):
+        callback_list = []
+        if use_early_stop:
+            # EarlyStopping
+            early_stopping = EarlyStopping(monitor='val_accuracy', patience=7, mode='max')
+            callback_list.append(early_stopping)
+        if checkpoint_path is not None:
+            # save model
+            checkpoint_dir = os.path.dirname(checkpoint_path)
+            checkout_dir(checkpoint_dir, do_delete=True)
+            # 创建一个保存模型权重的回调
+            cp_callback = ModelCheckpoint(filepath=checkpoint_path,
+                                             monitor='val_accuracy',
+                                             mode='max',
+                                             save_best_only=True,
+                                             save_weights_only=True,
+                                             verbose=1,
