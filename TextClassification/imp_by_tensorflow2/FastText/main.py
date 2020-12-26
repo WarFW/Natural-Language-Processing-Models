e5f8aa26d6c61e6bb47bcf6fd178e8dc0e586796
WarFW <106368492+WarFW@users.noreply.github.com>
2020-12-26 09:43:18 +0000

    Add Apache License

--- TextClassification/imp_by_tensorflow2/FastText/main.py
+++ TextClassification/imp_by_tensorflow2/FastText/main.py
@@ -110,3 +110,124 @@ class ModelHepler:
             cp_callback = ModelCheckpoint(filepath=checkpoint_path,
                                              monitor='val_accuracy',
                                              mode='max',
+                                             save_best_only=True,
+                                             save_weights_only=True,
+                                             verbose=1,
+                                             period=2,
+                                             )
+            callback_list.append(cp_callback)
+        if tensorboard_log_dir is not None:
+            # tensorboard --logdir logs/FastText-epoch-5
+            checkout_dir(tensorboard_log_dir, do_delete=True)
+            tensorboard_callback = TensorBoard(log_dir=tensorboard_log_dir, histogram_freq=1)
+            callback_list.append(tensorboard_callback)
+        self.callback_list = callback_list
+
+    def fit(self, x_train, y_train, x_val, y_val):
+        print('Train...')
+        self.model.fit(x_train, y_train,
+                  batch_size=self.batch_size,
+                  epochs=self.epochs,
+                  verbose=2,
+                  callbacks=self.callback_list,
+                  validation_data=(x_val, y_val))
+
+    def load_model(self, checkpoint_path):
+        checkpoint_dir = os.path.dirname((checkpoint_path))
+        latest = tf.train.latest_checkpoint(checkpoint_dir)
+        print('restore model name is : ', latest)
+        # 创建一个新的模型实例
+        # model = self.create_model()
+        # 加载以前保存的权重
+        self.model.load_weights(latest)
+
+# ================  params =========================
+ngram_range = 1
+class_num = 2
+maxlen = 400
+embedding_dims = 100
+epochs = 10
+batch_size = 128
+max_features = 5000
+
+MODEL_NAME = 'FastText-epoch-10-emb-100-maxpooling'
+
+use_early_stop=True
+tensorboard_log_dir = 'logs\\{}'.format(MODEL_NAME)
+# checkpoint_path = "save_model_dir\\{}\\cp-{epoch:04d}.ckpt".format(MODEL_NAME, '')
+checkpoint_path = 'save_model_dir\\'+MODEL_NAME+'\\cp-{epoch:04d}.ckpt'
+#  ====================================================================
+
+print('Loading data...')
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+print(len(x_train), 'train sequences')
+print(len(x_test), 'test sequences')
+print('Average train sequence length: {}'.format(np.mean(list(map(len, x_train)), dtype=int)))
+print('Average test sequence length: {}'.format(np.mean(list(map(len, x_test)), dtype=int)))
+
+if ngram_range > 1:
+    print('Adding {}-gram features'.format(ngram_range))
+    # Create set of unique n-gram from the training set.
+    ngram_set = set()
+    for input_list in x_train:
+        for i in range(2, ngram_range + 1):
+            set_of_ngram = create_ngram_set(input_list, ngram_value=i)
+            ngram_set.update(set_of_ngram)
+
+    # Dictionary mapping n-gram token to a unique integer.
+    # Integer values are greater than max_features in order
+    # to avoid collision with existing features.
+    start_index = max_features + 1
+    token_indice = {v: k + start_index for k, v in enumerate(ngram_set)}
+    indice_token = {token_indice[k]: k for k in token_indice}
+
+    # max_features is the highest integer that could be found in the dataset.
+    max_features = np.max(list(indice_token.keys())) + 1
+
+    # Augmenting x_train and x_test with n-grams features
+    x_train = add_ngram(x_train, token_indice, ngram_range)
+    x_test = add_ngram(x_test, token_indice, ngram_range)
+    print('Average train sequence length: {}'.format(np.mean(list(map(len, x_train)), dtype=int)))
+    print('Average test sequence length: {}'.format(np.mean(list(map(len, x_test)), dtype=int)))
+print('Pad sequences (samples x time)...')
+x_train = pad_sequences(x_train, maxlen=maxlen, padding='post')
+x_test = pad_sequences(x_test, maxlen=maxlen, padding='post')
+print('x_train shape:', x_train.shape)
+print('x_test shape:', x_test.shape)
+
+model_hepler = ModelHepler(class_num=class_num,
+                           maxlen=maxlen,
+                           max_features=max_features,
+                           embedding_dims=embedding_dims,
+                           epochs=epochs,
+                           batch_size=batch_size
+                           )
+model_hepler.get_callback(use_early_stop=use_early_stop, tensorboard_log_dir=tensorboard_log_dir, checkpoint_path=checkpoint_path)
+model_hepler.fit(x_train=x_train, y_train=y_train, x_val=x_test, y_val=y_test)
+print('Test...')
+result = model_hepler.model.predict(x_test)
+test_score = model_hepler.model.evaluate(x_test, y_test,
+                            batch_size=batch_size)
+print("test loss:", test_score[0], "test accuracy", test_score[1])
+
+
+
+model_hepler = ModelHepler(class_num=class_num,
+                           maxlen=maxlen,
+                           max_features=max_features,
+                           embedding_dims=embedding_dims,
+                           epochs=epochs,
+                           batch_size=batch_size
+                           )
+model_hepler.load_model(checkpoint_path=checkpoint_path)
+# 重新评估模型  0.8838
+loss, acc = model_hepler.model.evaluate(x_test, y_test, verbose=2)
+print("Restored model, accuracy: {:5.2f}%".format(100 * acc))
+
+
+
+
+
+
+
+
