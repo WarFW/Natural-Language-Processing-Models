39e51277dc6401e58e89dfc956a8a9434c0583de
WarFW <106368492+WarFW@users.noreply.github.com>
2020-12-26 16:14:39 +0000

    first file

--- TextClassification/imp_by_tensorflow2/TextBiRNN/main.py
+++ TextClassification/imp_by_tensorflow2/TextBiRNN/main.py
@@ -77,3 +77,83 @@ class ModelHepler:
                                              save_best_only=True,
                                              save_weights_only=True,
                                              verbose=1,
+                                             period=2,
+                                             )
+            callback_list.append(cp_callback)
+        if tensorboard_log_dir is not None:
+            # tensorboard --logdir logs/FastText-epoch-5
+            checkout_dir(tensorboard_log_dir, do_delete=True)
+            tensorboard_callback = TensorBoard(log_dir=tensorboard_log_dir, histogram_freq=1)
+            callback_list.append(tensorboard_callback)
+        self.callback_list = callback_list
+
+    def fit(self, x_train, y_train, x_val, y_val):
+        print('Train...')
+        self.model.fit(x_train, y_train,
+                  batch_size=self.batch_size,
+                  epochs=self.epochs,
+                  verbose=2,
+                  callbacks=self.callback_list,
+                  validation_data=(x_val, y_val))
+
+    def load_model(self, checkpoint_path):
+        checkpoint_dir = os.path.dirname((checkpoint_path))
+        latest = tf.train.latest_checkpoint(checkpoint_dir)
+        print('restore model name is : ', latest)
+        # 创建一个新的模型实例
+        # model = self.create_model()
+        # 加载以前保存的权重
+        self.model.load_weights(latest)
+
+# ================  params =========================
+class_num = 2
+maxlen = 400
+embedding_dims = 100
+epochs = 10
+batch_size = 256
+max_features = 5000
+
+MODEL_NAME = 'TextBiRNN-epoch-10-emb-100-avg2-dense2'
+
+use_early_stop=True
+tensorboard_log_dir = 'logs\\{}'.format(MODEL_NAME)
+# checkpoint_path = "save_model_dir\\{}\\cp-{epoch:04d}.ckpt".format(MODEL_NAME, '')
+checkpoint_path = 'save_model_dir\\'+MODEL_NAME+'\\cp-{epoch:04d}.ckpt'
+#  ====================================================================
+
+print('Loading data...')
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+
+print('Pad sequences (samples x time)...')
+x_train = pad_sequences(x_train, maxlen=maxlen, padding='post')
+x_test = pad_sequences(x_test, maxlen=maxlen, padding='post')
+print('x_train shape:', x_train.shape)
+print('x_test shape:', x_test.shape)
+
+model_hepler = ModelHepler(class_num=class_num,
+                           maxlen=maxlen,
+                           max_features=max_features,
+                           embedding_dims=embedding_dims,
+                           epochs=epochs,
+                           batch_size=batch_size
+                           )
+model_hepler.get_callback(use_early_stop=use_early_stop, tensorboard_log_dir=tensorboard_log_dir, checkpoint_path=checkpoint_path)
+model_hepler.fit(x_train=x_train, y_train=y_train, x_val=x_test, y_val=y_test)
+print('Test...')
+result = model_hepler.model.predict(x_test)
+test_score = model_hepler.model.evaluate(x_test, y_test,
+                            batch_size=batch_size)
+print("test loss:", test_score[0], "test accuracy", test_score[1])
+
+
+model_hepler = ModelHepler(class_num=class_num,
+                           maxlen=maxlen,
+                           max_features=max_features,
+                           embedding_dims=embedding_dims,
+                           epochs=epochs,
+                           batch_size=batch_size
+                           )
+model_hepler.load_model(checkpoint_path=checkpoint_path)
+# 重新评估模型  0.8790
+loss, acc = model_hepler.model.evaluate(x_test, y_test, verbose=2)
+print("Restored model, accuracy: {:5.2f}%".format(100 * acc))
---
+++ TextClassification/imp_by_tensorflow2/TextBiRNN/text_birnn.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/4/21 10:47
+# @Author : zdqzyx
+# @File : te
