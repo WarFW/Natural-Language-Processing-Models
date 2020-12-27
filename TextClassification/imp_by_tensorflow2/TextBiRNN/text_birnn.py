a9e3d4ae71d88d58650a4382068bf0a2cecce345
WarFW <106368492+WarFW@users.noreply.github.com>
2020-12-27 16:21:38 +0000

    width

--- TextClassification/imp_by_tensorflow2/TextBiRNN/text_birnn.py
+++ TextClassification/imp_by_tensorflow2/TextBiRNN/text_birnn.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
 # @Time : 2020/4/21 10:47
 # @Author : zdqzyx
-# @File : te
+# @File : text_birnn.py
+# @Software: PyCharm
+
+import  tensorflow as tf
+from tensorflow.keras.layers import Embedding, Dense, GRU, Bidirectional, GlobalAveragePooling1D
+fr
