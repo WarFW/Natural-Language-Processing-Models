2f0c876090dc018eecb944e706e45ab7f4f1fc8d
WarFW <106368492+WarFW@users.noreply.github.com>
2021-01-08 13:19:34 +0000

    USB: mos7720: fix message timeouts

--- TextClassification/imp_by_tensorflow2/TextBiRNNAtt/attention.py
+++ TextClassification/imp_by_tensorflow2/TextBiRNNAtt/attention.py
@@ -2,4 +2,10 @@
 # @Time : 2020/4/21 13:55
 # @Author : zdqzyx
 # @File : attention.py
-#
+# @Software: PyCharm
+
+from tensorflow.keras import  initializers,regularizers,constraints
+from  tensorflow.keras.layers import Layer
+import tensorflow as tf
+
+cl
