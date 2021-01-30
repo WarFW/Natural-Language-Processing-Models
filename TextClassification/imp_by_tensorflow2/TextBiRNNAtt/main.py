eaa18f12bf204f62c0b9ab6cec2b28f8e502fb84
WarFW <106368492+WarFW@users.noreply.github.com>
2021-01-30 19:21:16 +0000

    First Upload

--- TextClassification/imp_by_tensorflow2/TextBiRNNAtt/main.py
+++ TextClassification/imp_by_tensorflow2/TextBiRNNAtt/main.py
@@ -1,2 +1,9 @@
 # -*- coding: utf-8 -*-
-# @T
+# @Time : 2020/4/21 11:44
+# @Author : zdqzyx
+# @File : main.py
+# @Software: PyCharm
+
+
+# ===================== set random  ===========================
+import numpy 
