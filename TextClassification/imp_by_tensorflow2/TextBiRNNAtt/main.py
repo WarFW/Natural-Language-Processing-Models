ad35d58b30b848ae9ea88973ccd7d426d6ef1631
WarFW <106368492+WarFW@users.noreply.github.com>
2021-01-30 21:48:53 +0000

    Add more tests.

--- TextClassification/imp_by_tensorflow2/TextBiRNNAtt/main.py
+++ TextClassification/imp_by_tensorflow2/TextBiRNNAtt/main.py
@@ -6,4 +6,10 @@
 
 
 # ===================== set random  ===========================
-import numpy 
+import numpy as np
+import tensorflow as tf
+import random as rn
+np.random.seed(0)
+rn.seed(0)
+tf.random.set_seed(0)
+# ====================================================
