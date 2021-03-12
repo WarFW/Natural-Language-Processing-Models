df18a9babf6f24d27368b512e853e050de189403
WarFW <106368492+WarFW@users.noreply.github.com>
2021-03-12 10:44:36 +0000

    Fix search

--- TextClassification/imp_by_tensorflow2/TextBiRNNAtt/main.py
+++ TextClassification/imp_by_tensorflow2/TextBiRNNAtt/main.py
@@ -131,3 +131,7 @@ print('x_test shape:', x_test.shape)
 model_hepler = ModelHepler(class_num=class_num,
                            maxlen=maxlen,
                            max_features=max_features,
+                           embedding_dims=embedding_dims,
+                           epochs=epochs,
+                           batch_size=batch_size
+       
