07e5a7c69723f0a108f142093d2f51f31756bcbc
WarFW <106368492+WarFW@users.noreply.github.com>
2021-03-23 14:13:41 +0000

    upload 😎

--- TextClassification/imp_by_tensorflow2/TextBiRNNAtt/text_birnn_att.py
+++ TextClassification/imp_by_tensorflow2/TextBiRNNAtt/text_birnn_att.py
@@ -8,4 +8,9 @@
 import  tensorflow as tf
 from tensorflow.keras.layers import Embedding, Dense, GRU, Bidirectional
 from tensorflow.keras import Model
-from TextClassification.imp_by_tenso
+from TextClassification.imp_by_tensorflow2.TextBiRNNAtt.attention import Attention
+
+def point_wise_feed_forward_network(dense_size):
+    ffn = tf.keras.Sequential()
+    for size in dense_size:
+        ffn.add(Den
