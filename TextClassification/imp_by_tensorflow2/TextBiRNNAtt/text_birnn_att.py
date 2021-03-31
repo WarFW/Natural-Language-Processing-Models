3b66b05d1f8de6f5b0da89532a6469cbf641d2e1
WarFW <106368492+WarFW@users.noreply.github.com>
2021-03-31 12:44:49 +0000

    Revert "Add files via upload"

--- TextClassification/imp_by_tensorflow2/TextBiRNNAtt/text_birnn_att.py
+++ TextClassification/imp_by_tensorflow2/TextBiRNNAtt/text_birnn_att.py
@@ -65,3 +65,6 @@ class TextBiRNNAtt(Model):
     def build_graph(self, input_shape):
         input_shape_nobatch = input_shape[1:]
         self.build(input_shape)
+        inputs = tf.keras.Input(shape=input_shape_nobatch)
+        if not hasattr(self, 'call'):
+            raise AttributeError("User should define 'call' method in sub-
