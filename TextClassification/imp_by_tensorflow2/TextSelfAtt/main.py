72c26fc28aff207671c60b0321aa53e3c85fe52c
WarFW <106368492+WarFW@users.noreply.github.com>
2021-05-19 21:21:23 +0000

    readme link fix

--- TextClassification/imp_by_tensorflow2/TextSelfAtt/main.py
+++ TextClassification/imp_by_tensorflow2/TextSelfAtt/main.py
@@ -24,4 +24,11 @@ from TextClassification.imp_by_tensorflow2.TextSelfAtt.text_selfatt import TextS
 def checkout_dir(dir_path, do_delete=False):
     import shutil
     if do_delete and os.path.exists(dir_path):
-        shutil.rmtree(dir_path
+        shutil.rmtree(dir_path)
+    if not os.path.exists(dir_path):
+        print(dir_path, 'make dir ok')
+        os.makedirs(dir_path)
+
+
+class ModelHepler:
+    def __init__(self
