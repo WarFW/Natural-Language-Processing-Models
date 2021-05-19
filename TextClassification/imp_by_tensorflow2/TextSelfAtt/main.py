980700477dfed7dc792221f590a0b17ec82c27d3
WarFW <106368492+WarFW@users.noreply.github.com>
2021-05-19 16:51:49 +0000

    Added empty line

--- TextClassification/imp_by_tensorflow2/TextSelfAtt/main.py
+++ TextClassification/imp_by_tensorflow2/TextSelfAtt/main.py
@@ -18,4 +18,10 @@ import os
 from tensorflow.keras.callbacks import EarlyStopping, TensorBoard, ModelCheckpoint
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from TextClassification.imp_by_tensorflow2.TextSelfAtt.text_s
+from TextClassification.imp_by_tensorflow2.TextSelfAtt.text_selfatt import TextSelfAtt
+
+
+def checkout_dir(dir_path, do_delete=False):
+    import shutil
+    if do_delete and os.path.exists(dir_path):
+        shutil.rmtree(dir_path
