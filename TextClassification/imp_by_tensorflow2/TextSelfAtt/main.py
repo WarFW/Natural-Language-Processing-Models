f86e8944a3f6c6963a0b2c084390aaffb478c071
WarFW <106368492+WarFW@users.noreply.github.com>
2021-06-25 13:32:47 +0000

    Revert version number

--- TextClassification/imp_by_tensorflow2/TextSelfAtt/main.py
+++ TextClassification/imp_by_tensorflow2/TextSelfAtt/main.py
@@ -80,4 +80,7 @@ class ModelHepler:
                                              )
             callback_list.append(cp_callback)
         if tensorboard_log_dir is not None:
-            # tensorboard --logdi
+            # tensorboard --logdir logs/FastText-epoch-5
+            checkout_dir(tensorboard_log_dir, do_delete=True)
+            tensorboard_callback = TensorBoard(log_dir=tensorboard_log_dir, histogram_freq=1)
+          
