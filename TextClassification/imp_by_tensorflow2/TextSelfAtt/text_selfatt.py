3090854e63bfc10be8ea5bca913766fff6496507
WarFW <106368492+WarFW@users.noreply.github.com>
2021-08-01 12:31:52 +0000

    Add phpunit

--- TextClassification/imp_by_tensorflow2/TextSelfAtt/text_selfatt.py
+++ TextClassification/imp_by_tensorflow2/TextSelfAtt/text_selfatt.py
@@ -2,4 +2,10 @@
 # @Time : 2020/4/21 13:50
 # @Author : zdqzyx
 # @File : text_selfatt.py
-# @Software: Py
+# @Software: PyCharm
+
+
+import  tensorflow as tf
+from tensorflow.keras.layers import Embedding, Dense, GRU, Bidirectional, Flatten
+from tensorflow.keras import Model
+from
