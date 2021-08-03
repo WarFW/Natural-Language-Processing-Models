7708eaff4d45055f1e4c6203b991dc260ef2dd0e
WarFW <106368492+WarFW@users.noreply.github.com>
2021-08-03 07:43:42 +0000

    Publish page-18 index

--- TextClassification/imp_by_tensorflow2/TextSelfAtt/text_selfatt.py
+++ TextClassification/imp_by_tensorflow2/TextSelfAtt/text_selfatt.py
@@ -8,4 +8,8 @@
 import  tensorflow as tf
 from tensorflow.keras.layers import Embedding, Dense, GRU, Bidirectional, Flatten
 from tensorflow.keras import Model
-from
+from TextClassification.imp_by_tensorflow2.TextSelfAtt.attention import MultiHeadAttention
+
+def point_wise_feed_forward_network(dense_size):
+    ffn = tf.keras.Sequential()
+  
