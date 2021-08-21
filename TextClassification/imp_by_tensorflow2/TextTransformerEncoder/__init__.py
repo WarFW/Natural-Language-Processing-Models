96da571233901af4d5f4786001e08c8a1fc9722c
WarFW <106368492+WarFW@users.noreply.github.com>
2021-08-21 16:06:55 +0000

    Updated regression test results

--- TextClassification/imp_by_tensorflow2/TextTransformerEncoder/__init__.py
+++ TextClassification/imp_by_tensorflow2/TextTransformerEncoder/__init__.py
@@ -2,4 +2,4 @@
 # @Time : 2020/4/24 10:43
 # @Author : zdqzyx
 # @File : __init__.py.py
-# @Software: PyCha
+# @Software: PyCharm 
---
+++ TextClassification/imp_by_tensorflow2/TextTransformerEncoder/main.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/4/21 11:44
+# @Author : zdqzyx
+# @File : main.py
+# @Software: PyCharm
+
+
+# ===================== set random  ========
