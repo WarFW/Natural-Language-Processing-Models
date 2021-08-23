ec951d4cee842194c451e92206426586211b648b
WarFW <106368492+WarFW@users.noreply.github.com>
2021-08-23 16:06:11 +0000

    det

--- TextClassification/imp_by_tensorflow2/TextTransformerEncoder/main.py
+++ TextClassification/imp_by_tensorflow2/TextTransformerEncoder/main.py
@@ -5,4 +5,12 @@
 # @Software: PyCharm
 
 
-# ===================== set random  ========
+# ===================== set random  ===========================
+import numpy as np
+import tensorflow as tf
+import random as rn
+np.random.seed(0)
+rn.seed(0)
+tf.random.set_seed(0)
+
+# ==========
