631415034cf3f16852a7d44b652eec962fcf111b
WarFW <106368492+WarFW@users.noreply.github.com>
2021-08-26 13:12:37 +0000

    Security fixes

--- TextClassification/imp_by_tensorflow2/TextTransformerEncoder/main.py
+++ TextClassification/imp_by_tensorflow2/TextTransformerEncoder/main.py
@@ -25,4 +25,8 @@ def create_padding_mask(seq):
     seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
     # 添加额外的维度来将填充加到
     # 注意力对数（logits）。
-    
+    return seq[:, tf.newaxis, tf.newaxis, :]  # (batch_size, 1, 1, seq_len)
+
+def checkout_dir(dir_path, do_delete=False):
+    import shutil
+    if do_delete and os.path
