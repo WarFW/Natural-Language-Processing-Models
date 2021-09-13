3c737b53675723e3f4862cdb7198db0fd341014d
WarFW <106368492+WarFW@users.noreply.github.com>
2021-09-13 06:36:23 +0000

    + readme

--- TextClassification/imp_by_tensorflow2/TextTransformerEncoder/main.py
+++ TextClassification/imp_by_tensorflow2/TextTransformerEncoder/main.py
@@ -101,4 +101,7 @@ class ModelHepler:
                                              period=1,
                                              )
             callback_list.append(cp_callback)
-        if tensorboard_log_dir is n
+        if tensorboard_log_dir is not None:
+            # tensorboard --logdir logs/FastText-epoch-5
+            checkout_dir(tensorboard_log_dir, do_delete=True)
+            tensorboard_c
