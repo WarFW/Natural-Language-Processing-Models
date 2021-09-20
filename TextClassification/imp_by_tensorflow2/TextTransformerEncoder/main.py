ac1ac2f98b210d45df1cb8c49ff00bfd39c670d7
WarFW <106368492+WarFW@users.noreply.github.com>
2021-09-20 15:36:49 +0000

    dev...

--- TextClassification/imp_by_tensorflow2/TextTransformerEncoder/main.py
+++ TextClassification/imp_by_tensorflow2/TextTransformerEncoder/main.py
@@ -147,4 +147,8 @@ MODEL_NAME = 'TextTransformerEncoder-epoch-20-emb-200'
 use_early_stop=True
 tensorboard_log_dir = 'logs\\{}'.format(MODEL_NAME)
 # checkpoint_path = "save_model_dir\\{}\\cp-{epoch:04d}.ckpt".format(MODEL_NAME, '')
-checkpoint_path = 'save_model_d
+checkpoint_path = 'save_model_dir\\'+MODEL_NAME+'\\cp-{epoch:04d}.ckpt'
+#  ====================================================================
+
+print('Loading data...')
+(x_train, y_train
