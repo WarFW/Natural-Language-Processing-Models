71992c20468039dfda9407df0334194c73b45280
WarFW <106368492+WarFW@users.noreply.github.com>
2021-11-16 08:52:08 +0000

    Initial project upload

--- Transformer/imp_by_tensorflow20_custom/custom_main.py
+++ Transformer/imp_by_tensorflow20_custom/custom_main.py
@@ -18,4 +18,6 @@ def create_model(params, is_train):
         if is_train:
             inputs = tf.keras.layers.Input((None,), dtype='int64', name="inputs")
             targets = tf.keras.layers.Input((None,), dtype='int64', name="targets")
-            internal_model = Transformer(params
+            internal_model = Transformer(params,)
+            logits = internal_model([inputs, targets], training=is_train)
+            # logits = tf.keras.layers.Lambda(lambda x: x, name="logits", 
