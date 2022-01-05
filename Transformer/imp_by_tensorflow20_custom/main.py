a3cdeb5bc76bb9773b46da4dc5c7876eb18c33ef
WarFW <106368492+WarFW@users.noreply.github.com>
2022-01-05 16:17:07 +0000

    add monitor

--- Transformer/imp_by_tensorflow20_custom/main.py
+++ Transformer/imp_by_tensorflow20_custom/main.py
@@ -81,4 +81,10 @@ class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
         super(CustomSchedule, self).__init__()
 
         self.d_model = d_model
-        self.d_mo
+        self.d_model = tf.cast(self.d_model, tf.float32)
+
+        self.warmup_steps = warmup_steps
+
+    def __call__(self, step):
+        arg1 = tf.math.rsqrt(step)
+        arg2 = step * (
