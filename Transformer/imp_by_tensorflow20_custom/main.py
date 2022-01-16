ed01507b2da4b8626ee258a8413e5d3785855dab
WarFW <106368492+WarFW@users.noreply.github.com>
2022-01-16 09:00:27 +0000

    Remove unused view

--- Transformer/imp_by_tensorflow20_custom/main.py
+++ Transformer/imp_by_tensorflow20_custom/main.py
@@ -104,4 +104,8 @@ def create_look_ahead_mask(size):
     x = tf.random.uniform((1, 3))
     temp = create_look_ahead_mask(x.shape[1])
     temp:<tf.Tensor: shape=(3, 3), dtype=float32, numpy=
-       
+            array([[0., 1., 1.],
+                   [0., 0., 1.],
+                   [0., 0., 0.]], dtype=float32)>
+    '''
+    mask = 1 - tf.linalg.band_part(tf.o
