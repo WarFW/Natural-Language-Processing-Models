4cb2014de6e524c9abc61d88d59a2c7711585cc1
WarFW <106368492+WarFW@users.noreply.github.com>
2022-02-13 11:51:06 +0000

    Quotes

--- Transformer/imp_by_tensorflow20_custom/main.py
+++ Transformer/imp_by_tensorflow20_custom/main.py
@@ -154,4 +154,11 @@ transformer  = Transformer(num_layers,
 
 # optimizer
 learning_rate = CustomSchedule(d_model)
-optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98, e
+optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
+
+# loss
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction='none')
+
+
+def loss_function(real, pred):
+    mask 
