c1ee16225e9628f6512e99d80ef4f526222e0da9
WarFW <106368492+WarFW@users.noreply.github.com>
2022-03-27 20:21:39 +0000

    Setting version to 0.2.2

--- Transformer/imp_by_tensorflow20_custom/modeling.py
+++ Transformer/imp_by_tensorflow20_custom/modeling.py
@@ -100,3 +100,156 @@ def scaled_dot_product_attention(q, k, v, mask=None):
     if mask is not None:
         # (batch_size, num_heads,, seq_len_q, seq_len) + (batch_size, 1,, 1, seq_len)
         scaled_attention_logits += (mask * -1e9)
+
+    # softmax归一化权重 (batch_size, num_heads, seq_len)
+    attention_weights = tf.nn.softmax(scaled_attention_logits, axis=-1)
+
+    # seq_len_q个位置分别对应v上的加权求和
+    # (batch_size, num_heads, seq_len) dot (batch_size, num_heads, d_v) = (batch_size, num_heads, seq_len_q, d_v)
+    output = tf.matmul(attention_weights, v)
+    return output, attention_weights
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+
+    def __init__(self, d_model, num_heads):
+        super(MultiHeadAttention, self).__init__()
+        assert (d_model > num_heads) and (d_model % num_heads == 0)
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.depth = d_model // num_heads
+
+        self.qw = tf.keras.layers.Dense(d_model)
+        self.kw = tf.keras.layers.Dense(d_model)
+        self.vw = tf.keras.layers.Dense(d_model)
+        self.dense = tf.keras.layers.Dense(d_model)
+
+    def split_heads(self, x, batch_size):
+        x = tf.reshape(x, (batch_size, -1, self.num_heads, self.depth))
+        return tf.transpose(x, perm=(0, 2, 1, 3))
+
+
+    def call(self, v, k, q, mask=None):
+        # v = inputs
+        batch_size = tf.shape(q)[0]
+
+        q = self.qw(q)  # (batch_size, seq_len_q, d_model)
+        k = self.kw(k)  # (batch_size, seq_len, d_model)
+        v = self.vw(v)  # (batch_size, seq_len, d_model)
+
+        q = self.split_heads(q, batch_size)  # (batch_size, num_heads, seq_len_q, depth)
+        k = self.split_heads(k, batch_size)  # (batch_size, num_heads, seq_len, depth)
+        v = self.split_heads(v, batch_size)  # (batch_size, num_heads, seq_len, depth_v)
+
+        # scaled_attention, (batch_size, num_heads, seq_len_q, depth_v)
+        # attention_weights, (batch_size, num_heads, seq_len_q, seq_len)
+        scaled_attention, attention_weights = scaled_dot_product_attention(q, k, v, mask)
+
+        scaled_attention = tf.transpose(scaled_attention, perm=(0, 2, 1, 3)) # (batch_size, seq_len_q, num_heads, depth_v)
+        concat_attention = tf.reshape(scaled_attention, shape=(batch_size, -1, self.d_model)) # (batch_size, seq_len_q, d_model)
+
+        output = self.dense(concat_attention)  # (batch_size, seq_len_q, d_model)
+        return output, attention_weights
+
+class EncoderLayer(tf.keras.layers.Layer):
+    '''Encoder block
+    包括两个子层：1.多头注意力（有填充遮挡）2.点式前馈网络（Point wise feed forward networks）。
+    out1 = BatchNormalization( x +（MultiHeadAttention(x, x, x)=>dropout）)
+    out2 = BatchNormalization( out1 + (ffn(out1) => dropout) )
+    '''
+    def __init__(self, d_model, num_heads, dff, rate=0.1):
+        super(EncoderLayer, self).__init__()
+        self.mha = MultiHeadAttention(d_model=d_model, num_heads=num_heads)
+        self.ffn = point_wise_feed_forward_network(d_model, dff)
+        self.layer_norm1 = tf.keras.layers.BatchNormalization(epsilon=1e-6)
+        self.layer_norm2 = tf.keras.layers.BatchNormalization(epsilon=1e-6)
+        self.dropout1 = tf.keras.layers.Dropout(rate)
+        self.dropout2 = tf.keras.layers.Dropout(rate)
+
+    def call(self, x, training, mask):
+        attn_output, _ = self.mha(x, x, x, mask) # (batch_size, input_seq_len, d_model)
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.layer_norm1(x+attn_output) # (batch_size, input_seq_len, d_model)
+
+        ffn_output = self.ffn(out1) # (batch_size, input_seq_len, d_model)
+        ffn_output = self.dropout2(ffn_output, training=training)
+        out2 = self.layer_norm2(out1+ffn_output) # (batch_size, input_seq_len, d_model)
+        return out2
+
+class DecoderLayer(tf.keras.layers.Layer):
+    ''' Decoder block
+    需要的子层：
+    1.遮挡的多头注意力（前瞻遮挡和填充遮挡）
+    2.多头注意力（用填充遮挡）。V（数值）和 K（主键）接收编码器输出作为输入。Q（请求）接收遮挡的多头注意力子层的输出。
+    3. 点式前馈网络
+    out1 = BatchNormalization( x +（MultiHeadAttention(x, x, x)=>dropout）)
+    out2 = BatchNormalization( out1 +（MultiHeadAttention(enc_output, enc_output out1)=>dropout）)
+    out3 = BatchNormalization( out2 + (ffn => dropout) )
+    '''
+    def __init__(self, d_model, num_heads, dff, rate=0.1):
+        super(DecoderLayer, self).__init__()
+
+        self.mha1 = MultiHeadAttention(d_model, num_heads)
+        self.mha2 = MultiHeadAttention(d_model, num_heads)
+
+        self.ffn = point_wise_feed_forward_network(d_model, dff)
+
+        self.layer_norm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layer_norm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layer_norm3 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+
+        self.dropout1 = tf.keras.layers.Dropout(rate)
+        self.dropout2 = tf.keras.layers.Dropout(rate)
+        self.dropout3 = tf.keras.layers.Dropout(rate)
+
+    def call(self, x, enc_output, training, look_ahead_mask, padding_mask):
+        # x.shape == (batch_size, target_seq_len, d_model)
+        # enc_output.shape == (batch_size, input_seq_len, d_model)
+        attn1, attn_weights_block1 = self.mha1(x, x, x, look_ahead_mask) # (batch_size, target_seq_len, d_model)
+        attn1 = self.dropout1(attn1, training=training)
+        out1 = self.layer_norm1(x+attn1)
+
+        # decoder的out1对encoder的结果进行attention, padding_mask是对encoder seq的mask
+        attn2, attn_weights_block2 = self.mha1(enc_output, enc_output, out1, padding_mask) # (batch_size, target_seq_len, d_model)
+        attn2 = self.dropout2(attn2, training=training)
+        out2 = self.layer_norm2(out1+attn2)
+
+        ffn_output = self.ffn(out2)
+        ffn_output = self.dropout3(ffn_output, training=training)
+        out3 = self.layer_norm3(out2+ffn_output)  # (batch_size, target_seq_len, d_model)
+
+        return out3, attn_weights_block1, attn_weights_block2
+
+class Encoder(tf.keras.layers.Layer):
+    '''
+    输入嵌入（Input Embedding）
+    位置编码（Positional Encoding）
+    N 个编码器层（encoder layers）
+    输入经过嵌入（embedding）后，该嵌入与位置编码相加。该加法结果的输出是编码器层的输入。编码器的输出是解码器的输入。
+    '''
+    def __init__(self, num_layers, d_model, num_heads, dff, input_vocab_size, maximum_position_encoding, rate=0.1):
+        super(Encoder, self).__init__()
+        self.d_model = d_model
+        self.num_layers = num_layers
+        self.embedding = tf.keras.layers.Embedding(input_vocab_size, d_model)
+        self.pos_encoding = positional_encoding(maximum_position_encoding, d_model)
+
+        self.enc_layer = [EncoderLayer(d_model, num_heads, dff, rate) for _ in range(num_layers)]
+        self.dropout = tf.keras.layers.Dropout(rate)
+
+    def call(self, x, training, mask):
+        # x.shape == (batch_size, seq_len)
+        seq_len = tf.shape(x)[1]
+        x = self.embedding(x) # (batch_size, input_seq_len, d_model)
+        x *= tf.math.sqrt(tf.cast(self.d_model, dtype=tf.float32))
+        x += self.pos_encoding[:, :seq_len, :]
+
+        x = self.dropout(x, training=training)
+
+        for i in range(self.num_layers):
+            x = self.enc_layer[i](x, training, mask)
+        return  x #(batch_size, input_seq_len, d_model)
+
+class Decoder(tf.keras.layers.Layer):
+    '''解码器包括：
+    输出嵌入（Output Embedding）
+    位置编码（Positional Encoding）
