e340bb2d113df7e78e8d918786934f08c6e6c848
WarFW <106368492+WarFW@users.noreply.github.com>
2022-05-02 19:05:19 +0000

    Updated installation docs

--- Transformer/imp_by_tensorflow20_custom_2/main.py
+++ Transformer/imp_by_tensorflow20_custom_2/main.py
@@ -86,4 +86,10 @@ class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
         self.warmup_steps = warmup_steps
 
     def __call__(self, step):
-      
+        arg1 = tf.math.rsqrt(step)
+        arg2 = step * (self.warmup_steps ** -1.5)
+
+        return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+
+
+def
