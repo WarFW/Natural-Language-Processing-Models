bc2e89f71ca21741f4f06a13989b5384a5d65718
WarFW <106368492+WarFW@users.noreply.github.com>
2022-05-07 20:46:01 +0000

    Refactored tests

--- Transformer/imp_by_tensorflow20_custom_2/main.py
+++ Transformer/imp_by_tensorflow20_custom_2/main.py
@@ -96,4 +96,11 @@ def create_padding_mask(seq):
     seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
     # 添加额外的维度来将填充加到
     # 注意力对数（logits）。
-    return seq[:, tf.newaxis, tf.newaxis, :]
+    return seq[:, tf.newaxis, tf.newaxis, :]  # (batch_size, 1, 1, seq_len)
+
+def create_look_ahead_mask(size):
+    '''
+    eg.
+    x = tf.random.uniform((1, 3))
+    temp = create_look_ahead_mask(x.shape[1])
+    tem
