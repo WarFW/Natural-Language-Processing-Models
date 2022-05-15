4b0f1dcf85f2a08e54452f989df215222aac7fa1
WarFW <106368492+WarFW@users.noreply.github.com>
2022-05-15 15:06:02 +0000

    closes #5

--- Transformer/imp_by_tensorflow20_custom_2/main.py
+++ Transformer/imp_by_tensorflow20_custom_2/main.py
@@ -130,3 +130,8 @@ def create_masks(inp, tar):
 # ==============================================================
 params = {
     'num_layers':4,
+    'd_model':128,
+    'dff':512,
+    'num_heads':8,
+    'input_vocab_size' :tokenizer_pt.vocab_size + 2,
+    'target_vocab_size':tokenizer_en.vocab_size + 
