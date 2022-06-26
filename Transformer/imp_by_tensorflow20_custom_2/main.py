4c158009ab6e01fbab2b44d08e6dae16b1db903d
WarFW <106368492+WarFW@users.noreply.github.com>
2022-06-26 18:19:46 +0000

    Rebuild against updated base image

--- Transformer/imp_by_tensorflow20_custom_2/main.py
+++ Transformer/imp_by_tensorflow20_custom_2/main.py
@@ -241,4 +241,8 @@ class ModelHelper:
 
         # 因为目标是英语，输入 transformer 的第一个词应该是
         # 英语的开始标记。
-        decoder_input = [tokenizer_en.vocab
+        decoder_input = [tokenizer_en.vocab_size]
+        output = tf.expand_dims(decoder_input, 0)
+
+        for i in range(MAX_LENGTH):
+            enc_padding_mask, combined_mask, dec_padding_ma
