4cddb37c9976571d24bfdcd03495e1315109a917
WarFW <106368492+WarFW@users.noreply.github.com>
2022-07-16 12:55:32 +0000

    links updated

--- Transformer/imp_by_tensorflow20_custom_2/modeling.py
+++ Transformer/imp_by_tensorflow20_custom_2/modeling.py
@@ -101,3 +101,202 @@ class MultiHeadAttention(tf.keras.layers.Layer):
         q = self.qw(q)  # (batch_size, seq_len_q, d_model)
         k = self.kw(k)  # (batch_size, seq_len, d_model)
         v = self.vw(v)  # (batch_size, seq_len, d_model)
+
+        q = self.split_heads(q, batch_size)  # (batch_size, num_heads, seq_len_q, depth)
+        k = self.split_heads(k, batch_size)  # (batch_size, num_heads, seq_len, depth)
+        v = self.split_heads(v, batch_size)  # (batch_size, num_heads, seq_len, depth_v)
+
+        # scaled_attention, (batch_size, num_heads, seq_len_q, depth_v)
+        # attention_weights, (batch_size, num_heads, seq_len_q, seq_len)
+        scaled_attention, attention_weights = scaled_dot_product_attention(q, k, v, mask)
+
+        scaled_attention = tf.transpose(scaled_attention, perm=(0, 2, 1, 3)) # (batch_size, seq_len_q, num_heads, depth_v)
+        concat_attention = tf.reshape(scaled_attention, shape=(batch_size, -1, self.d_model)) # (batch_size, seq_len_q, d_model)
+
+        output = self.dense(concat_attention)  # (batch_size, seq_len_q, d_model)
+        return output, attention_weights
+
+class EncoderLayer(tf.keras.layers.Layer):
+    '''Encoder block
+    包括两个子层：1.多头注意力（有填充遮挡）2.点式前馈网络（Point wise feed forward networks）。
+    out1 = BatchNormalization( x +（MultiHeadAttention(x, x, x)=>dropout）)
+    out2 = BatchNormalization( out1 + (ffn(out1) => dropout) )
+    '''
+    def __init__(self, d_model, num_heads, dff, rate=0.1):
+        super(EncoderLayer, self).__init__()
+        self.mha = MultiHeadAttention(d_model=d_model, num_heads=num_heads)
+        self.ffn = point_wise_feed_forward_network(d_model, dff)
+        self.layer_norm1 = tf.keras.layers.BatchNormalization(epsilon=1e-6)
+        self.layer_norm2 = tf.keras.layers.BatchNormalization(epsilon=1e-6)
+        self.dropout1 = tf.keras.layers.Dropout(rate)
+        self.dropout2 = tf.keras.layers.Dropout(rate)
+
+    def call(self, x, training, mask):
+        attn_output, _ = self.mha(x, x, x, mask) # (batch_size, input_seq_len, d_model)
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.layer_norm1(x+attn_output) # (batch_size, input_seq_len, d_model)
+
+        ffn_output = self.ffn(out1) # (batch_size, input_seq_len, d_model)
+        ffn_output = self.dropout2(ffn_output, training=training)
+        out2 = self.layer_norm2(out1+ffn_output) # (batch_size, input_seq_len, d_model)
+        return out2
+
+class DecoderLayer(tf.keras.layers.Layer):
+    ''' Decoder block
+    需要的子层：
+    1.遮挡的多头注意力（前瞻遮挡和填充遮挡）
+    2.多头注意力（用填充遮挡）。V（数值）和 K（主键）接收编码器输出作为输入。Q（请求）接收遮挡的多头注意力子层的输出。
+    3. 点式前馈网络
+    out1 = BatchNormalization( x +（MultiHeadAttention(x, x, x)=>dropout）)
+    out2 = BatchNormalization( out1 +（MultiHeadAttention(enc_output, enc_output out1)=>dropout）)
+    out3 = BatchNormalization( out2 + (ffn => dropout) )
+    '''
+    def __init__(self, d_model, num_heads, dff, rate=0.1):
+        super(DecoderLayer, self).__init__()
+
+        self.mha1 = MultiHeadAttention(d_model, num_heads)
+        self.mha2 = MultiHeadAttention(d_model, num_heads)
+
+        self.ffn = point_wise_feed_forward_network(d_model, dff)
+
+        self.layer_norm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layer_norm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layer_norm3 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+
+        self.dropout1 = tf.keras.layers.Dropout(rate)
+        self.dropout2 = tf.keras.layers.Dropout(rate)
+        self.dropout3 = tf.keras.layers.Dropout(rate)
+
+    def call(self, x, enc_output, training, look_ahead_mask, padding_mask):
+        # x.shape == (batch_size, target_seq_len, d_model)
+        # enc_output.shape == (batch_size, input_seq_len, d_model)
+        attn1, attn_weights_block1 = self.mha1(x, x, x, look_ahead_mask) # (batch_size, target_seq_len, d_model)
+        attn1 = self.dropout1(attn1, training=training)
+        out1 = self.layer_norm1(x+attn1)
+
+        attn2, attn_weights_block2 = self.mha1(enc_output, enc_output, out1, padding_mask) # (batch_size, target_seq_len, d_model)
+        attn2 = self.dropout2(attn2, training=training)
+        out2 = self.layer_norm2(out1+attn2)
+
+        ffn_output = self.ffn(out2)
+        ffn_output = self.dropout3(ffn_output, training=training)
+        out3 = self.layer_norm3(out2+ffn_output)  # (batch_size, target_seq_len, d_model)
+
+        return out3, attn_weights_block1, attn_weights_block2
+
+class Encoder(tf.keras.layers.Layer):
+    '''
+    输入嵌入（Input Embedding）
+    位置编码（Positional Encoding）
+    N 个编码器层（encoder layers）
+    输入经过嵌入（embedding）后，该嵌入与位置编码相加。该加法结果的输出是编码器层的输入。编码器的输出是解码器的输入。
+    '''
+    def __init__(self, num_layers, d_model, num_heads, dff, input_vocab_size, maximum_position_encoding, rate=0.1):
+        super(Encoder, self).__init__()
+        self.d_model = d_model
+        self.num_layers = num_layers
+        self.embedding = tf.keras.layers.Embedding(input_vocab_size, d_model)
+        self.pos_encoding = positional_encoding(maximum_position_encoding, d_model)
+
+        self.enc_layer = [EncoderLayer(d_model, num_heads, dff, rate) for _ in range(num_layers)]
+        self.dropout = tf.keras.layers.Dropout(rate)
+
+    def call(self, x, training, mask):
+        # x.shape == (batch_size, seq_len)
+        seq_len = tf.shape(x)[1]
+        x = self.embedding(x) # (batch_size, input_seq_len, d_model)
+        x *= tf.math.sqrt(tf.cast(self.d_model, dtype=tf.float32))
+        x += self.pos_encoding[:, :seq_len, :]
+
+        x = self.dropout(x, training=training)
+
+        for i in range(self.num_layers):
+            x = self.enc_layer[i](x, training, mask)
+        return  x #(batch_size, input_seq_len, d_model)
+
+class Decoder(tf.keras.layers.Layer):
+    '''解码器包括：
+    输出嵌入（Output Embedding）
+    位置编码（Positional Encoding）
+    N 个解码器层（decoder layers）
+    目标（target）经过一个嵌入后，该嵌入和位置编码相加。该加法结果是解码器层的输入。解码器的输出是最后的线性层的输入。
+    '''
+    def __init__(self, num_layers, d_model, num_heads, dff, target_vocab_size, maximum_position_encoding, rate=0.1):
+        super(Decoder, self).__init__()
+        self.d_model = d_model
+        self.num_layers = num_layers
+
+        self.embedding = tf.keras.layers.Embedding(target_vocab_size, d_model)
+        self.pos_encoding = positional_encoding(maximum_position_encoding, d_model)
+        self.dec_layer = [DecoderLayer(d_model, num_heads, dff, rate) for _ in range(num_layers)]
+        self.dropout = tf.keras.layers.Dropout(rate)
+
+    def call(self, x, enc_output, training, look_ahead_mask, padding_mask):
+        # x.shape==(batch_size, target_seq_len)
+        # enc_output.shape==(batch_size, input_seq_len, d_model)
+        seq_len = tf.shape(x)[1]
+        attention_weights = {}
+
+        x = self.embedding(x) # (batch_size, target_seq_len, d_model)
+        x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
+        x += self.pos_encoding[:, :seq_len, :]
+        x = self.dropout(x, training=training)
+
+        for i in range(self.num_layers):
+            x, block1, block2 = self.dec_layer[i](x, enc_output, training, look_ahead_mask, padding_mask)
+            attention_weights['decoder_layer{}_block1'.format(i + 1)] = block1
+            attention_weights['decoder_layer{}_block2'.format(i + 1)] = block2
+        # x.shape==(batch_size, target_seq_len, d_model)
+        return x, attention_weights
+
+
+class Transformer(tf.keras.Model):
+    def __init__(self, params):
+        super(Transformer, self).__init__()
+        self.encoder = Encoder(params['num_layers'],params['d_model'],params['num_heads'],params['dff'],params['input_vocab_size'],params['pe_input'],params['rate'])
+        self.decoder = Decoder(params['num_layers'],params['d_model'],params['num_heads'],params['dff'],params['target_vocab_size'],params['pe_target'],params['rate'])
+        self.final_layer = tf.keras.layers.Dense(params['target_vocab_size'])
+
+    def call(self, inp, tar, training, enc_padding_mask=None, look_ahead_mask=None, dec_padding_mask=None):
+        # (batch_size, inp_seq_len, d_model)
+        enc_output = self.encoder(inp, training, enc_padding_mask)
+        # (batch_size, tar_seq_len, d_model)
+        dec_output, attention_weights = self.decoder(tar, enc_output, training, look_ahead_mask, dec_padding_mask)
+        final_output = self.final_layer(dec_output)  # (batch_size, tar_seq_len, target_vocab_size)
+        return final_output, attention_weights
+
+    def build_graph(self, input_shape, target_shape):
+        input_shape_nobatch = input_shape[1:]
+        self.build(input_shape)
+        inputs = tf.keras.Input(shape=input_shape_nobatch)
+        if not hasattr(self, 'call'):
+            raise AttributeError("User should define 'call' method in sub-class model!")
+        _ = self.call(inputs)
+
+
+
+if __name__=='__main__':
+    parmas = {
+    'num_layers':2,
+    'd_model':512,
+    'num_heads' :8,
+    'dff' :2048,
+    'input_vocab_size' : 8500,
+    'target_vocab_size' :8000,
+    'pe_input' :10000,
+    'pe_target' : 6000,
+    'rate':0.1
+    }
+
+    model = Transformer(parmas)
+    # model.build_graph(input_shape=(None, 400))
+    # model.summary()
+    temp_input = tf.random.uniform((64, 62))
+    temp_target = tf.random.uniform((64, 26))
+
+    fn_out, _ = model(temp_input, temp_target, training=True,
+                                   enc_padding_mask=None,
+                                   look_ahead_mask=None,
+                                   dec_padding_mask=None)
+    print(fn_out.shape)
+
