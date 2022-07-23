a7520df5e4e73f5f37b407273adf24a0e64da082
WarFW <106368492+WarFW@users.noreply.github.com>
2022-07-23 08:32:17 +0000

    crash

--- Transformer/imp_by_tensorflow20_official/main.py
+++ Transformer/imp_by_tensorflow20_official/main.py
@@ -14,4 +14,7 @@ import os
 
 examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en', with_info=True,
                                as_supervised=True)
-train_examples, val_exampl
+train_examples, val_examples = examples['train'], examples['validation']
+
+tokenizer_en = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+    (en.numpy() for pt, en in train_exampl
