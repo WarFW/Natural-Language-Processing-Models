766a45338d9b75d015e0042b5d88a8301308e405
WarFW <106368492+WarFW@users.noreply.github.com>
2022-08-13 11:07:47 +0000

    dependency version bump

--- Transformer/imp_by_tensorflow20_official/main.py
+++ Transformer/imp_by_tensorflow20_official/main.py
@@ -93,4 +93,9 @@ class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
 
 
 def create_padding_mask(seq):
-    seq = tf.cast
+    seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
+    # 添加额外的维度来将填充加到
+    # 注意力对数（logits）。
+    return seq[:, tf.newaxis, tf.newaxis, :]  # (batch_size, 1, 1, seq_len)
+
+def cre
