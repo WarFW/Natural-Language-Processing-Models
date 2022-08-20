25adf00db051586ca0c965593058567a2e3d18b8
WarFW <106368492+WarFW@users.noreply.github.com>
2022-08-20 10:43:52 +0000

    query

--- Transformer/imp_by_tensorflow20_official/main.py
+++ Transformer/imp_by_tensorflow20_official/main.py
@@ -119,4 +119,5 @@ def create_masks(inp, tar):
     dec_padding_mask = create_padding_mask(inp)
     # 在解码器的第一个注意力模块使用。
     # 用于填充（pad）和遮挡（mask）解码器获取到的输入的后续标记（future tokens）。
-    look_a
+    look_ahead_mask = create_look_ahead_mask(tf.shape(tar)[1]) #(tar_seq_len, tar_seq_len)
+    dec_target_padding_mask = create_padding_mask(tar) # (batch_size, 1, 1, tar_seq_le
