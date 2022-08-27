ea7c2dcbf466a3a3502ee360c16a355e992bdb5c
WarFW <106368492+WarFW@users.noreply.github.com>
2022-08-27 21:32:51 +0000

    - переименование

--- Transformer/imp_by_tensorflow20_official/main.py
+++ Transformer/imp_by_tensorflow20_official/main.py
@@ -135,4 +135,6 @@ num_layers = 4
 d_model = 128
 dff = 512
 num_heads = 8
-input_vocab_size = t
+input_vocab_size = tokenizer_pt.vocab_size + 2
+target_vocab_size = tokenizer_en.vocab_size + 2
+print('input_vocab_size is {}, target_vocab_size is {}'.format(input_vocab_size, target_vocab_size
