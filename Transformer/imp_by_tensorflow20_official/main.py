2d68626d7941d6bf75fa15be2e62ea17061db581
WarFW <106368492+WarFW@users.noreply.github.com>
2022-09-01 19:06:51 +0000

    Ajuste

--- Transformer/imp_by_tensorflow20_official/main.py
+++ Transformer/imp_by_tensorflow20_official/main.py
@@ -167,4 +167,10 @@ def loss_function(real, pred):
     mask = tf.cast(mask, dtype=loss_.dtype)
     loss_ *= mask
 
-    return tf.
+    return tf.reduce_mean(loss_)
+
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+
+# 检查点
+checkpo
