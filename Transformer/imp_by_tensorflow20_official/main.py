76d237fd4300cb61606acb8fc0175b71027fb0d9
WarFW <106368492+WarFW@users.noreply.github.com>
2022-09-04 10:49:19 +0000

    mm: frontswap: invalidate expired data on a dup-store failure

--- Transformer/imp_by_tensorflow20_official/main.py
+++ Transformer/imp_by_tensorflow20_official/main.py
@@ -176,4 +176,7 @@ train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy
 checkpoint_path = "./checkpoints/train"
 # checkout_dir(dir_path=checkpoint_path, do_delete=True)
 ckpt = tf.train.Checkpoint(transformer=transformer,
-   
+                           optimizer=optimizer)
+ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
+# 如果检查点存在，则恢复最新的检查点。
+if ckpt_manag
