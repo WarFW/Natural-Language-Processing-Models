38a06d3a2a386ecb367d60539979d3b52e51a142
WarFW <106368492+WarFW@users.noreply.github.com>
2022-10-19 17:14:43 +0000

    doh.

--- Transformer/imp_by_tensorflow20_official/main.py
+++ Transformer/imp_by_tensorflow20_official/main.py
@@ -311,4 +311,8 @@ def plot_attention_weights(attention, sentence, result, layer):
 
         ax.set_xticklabels(
             ['<start>'] + [tokenizer_pt.decode([i]) for i in sentence] + ['<end>'],
-            fontdict=fontdict, rota
+            fontdict=fontdict, rotation=90)
+
+        ax.set_yticklabels([tokenizer_en.decode([i]) for i in result
+                            if i < tokenizer_en.vocab_size],
+   
