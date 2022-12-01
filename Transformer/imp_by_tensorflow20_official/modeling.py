d0dcfac285b3463d5a09380cf18c98a1ec695d77
WarFW <106368492+WarFW@users.noreply.github.com>
2022-12-01 07:32:08 +0000

    [ZF-6295] Generic:

--- Transformer/imp_by_tensorflow20_official/modeling.py
+++ Transformer/imp_by_tensorflow20_official/modeling.py
@@ -1,1 +1,10 @@
-# -*- coding: 
+# -*- coding: utf-8 -*-
+# @Time : 2020/4/22 16:28
+# @Author : zdqzyx
+# @File : modeling.py
+# @Software: PyCharm
+
+import tensorflow as tf
+import  numpy as np
+
+def g
