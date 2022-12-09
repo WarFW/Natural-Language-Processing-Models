8fe156b00424ba26e02c196e00d6c4b84530a752
WarFW <106368492+WarFW@users.noreply.github.com>
2022-12-09 08:02:05 +0000

    unit tests extended

--- Transformer/imp_by_tensorflow20_official/modeling.py
+++ Transformer/imp_by_tensorflow20_official/modeling.py
@@ -35,4 +35,6 @@ def positional_encoding(position, d_model):
 
 def point_wise_feed_forward_network(d_model, dff):
   return tf.keras.Sequential([
-    
+      tf.keras.layers.Dense(dff, activation='relu'),  # (batch_size, seq_len, dff)
+      tf.keras.layers.Dense(d_model)  # (batch_size, seq_len, d_model)
+ 
