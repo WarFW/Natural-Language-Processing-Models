c652ea678d66773f37ed98f906a3333efa8ddd44
WarFW <106368492+WarFW@users.noreply.github.com>
2023-05-19 21:43:10 +0000

    bump version to 0.8.1

--- Transformer/imp_by_tensorflow20_official/modeling.py
+++ Transformer/imp_by_tensorflow20_official/modeling.py
@@ -135,4 +135,6 @@ class EncoderLayer(tf.keras.layers.Layer):
         attn_output = self.dropout1(attn_output, training=training)
         out1 = self.layer_norm1(x+attn_output) # (batch_size, input_seq_len, d_model)
 
-        ffn_o
+        ffn_output = self.ffn(out1) # (batch_size, input_seq_len, d_model)
+        ffn_output = self.dropout2(ffn_output, training=training)
+        out2 = self.layer_norm2(out1+f
