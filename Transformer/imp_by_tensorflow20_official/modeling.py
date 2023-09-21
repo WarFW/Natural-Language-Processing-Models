63ace65f0d44da3a43cb6e79b82f0cca2311b7eb
WarFW <106368492+WarFW@users.noreply.github.com>
2023-09-21 23:11:26 +0000

    Add favicon.ico

--- Transformer/imp_by_tensorflow20_official/modeling.py
+++ Transformer/imp_by_tensorflow20_official/modeling.py
@@ -259,4 +259,6 @@ class Transformer(tf.keras.Model):
     def call(self, inp, tar, training, enc_padding_mask=None, look_ahead_mask=None, dec_padding_mask=None):
         # (batch_size, inp_seq_len, d_model)
         enc_output = self.encoder(inp, training, enc_padding_mask)
-        # (batch_size, tar_seq
+        # (batch_size, tar_seq_len, d_model)
+        dec_output, attention_weights = self.decoder(tar, enc_output, training, look_ahead_mask, dec_padding_mask)
+        final_output = self.final_layer(dec_output)  #
